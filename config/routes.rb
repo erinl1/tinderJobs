ee70fae185ba8482e90b1627621d15595307644b
Erin Lee <erinleeny@gmail.com>
2018-10-12 20:46:53 -0400

    users

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :users
   resources :jobs
   resources :companies
+  root to: 'users#splash'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
