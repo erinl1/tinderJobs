75bea77bbbcd4b2dbbd930bf3ff0a3d32cbb144e
gescalona@browning.edu <george596ab@gmail.com>
2018-10-19 21:05:15 -0400

    start of nested jobs within companies

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,8 +1,3 @@
 class Company < ApplicationRecord
-<<<<<<< HEAD
-  has_many :job
-=======
   has_many :jobs
-  
->>>>>>> 5bf0b2d9123b436cb65bb656bae2b411aeab2ebe
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :users
-  resources :jobs
-  resources :companies
+  resources :companies do
+    resources :jobs
+  end
   root to: 'users#splash'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
