222fe523101dd6fc585d786a563319eaea9b42dc
Erin Lee <erinleeny@gmail.com>
2018-10-26 20:19:05 -0400

    edits

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  #layout "usernew",  only: [:new, :edit]
+  layout "usernew",  only: [:new, :edit]
   # GET /users
   # GET /users.json
   def index
@@ -32,6 +32,7 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
+
     respond_to do |format|
       if @user.save
         format.html { redirect_to @user, notice: 'User was successfully created.' }
